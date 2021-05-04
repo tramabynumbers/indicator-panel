e073aaa32345abb7c7df48f2ae00ab2f8bd685a7
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-04 19:42:29 -0300

    Adjust for new data set

--- js/start-map.js
+++ js/start-map.js
@@ -8,7 +8,7 @@ var mainMap={
     info:L.control(),
 
     init:()=>{
-        mainMap.map = L.map('mainmap').setView([-23, -45], 8);
+        mainMap.map = L.map('mainmap').setView([-6.9, -36.456], 7);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
@@ -115,7 +115,7 @@ var mainMap={
     },
 
     loadGeojson: async ()=>{
-        const response = await fetch("data/rm-vale.geojson");
+        const response = await fetch("data/munis-PB.geojson");
         const data = await response.json();
         mainMap.geojson = data;
         mainMap.renewMainLayer(data);
