ce057a26d6a4556835bf78b0afcdccae7ebc58db
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-25 22:34:09 -0300

    The modal window to display a PDF file

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,12 @@
 var utils={
+    mainRowOffset:0.6,// default offset to define height for main row
+
     /**
      * Apply a percentage of the total height to components in main row where is the map component.
      * @param {double} offset, a percentage value between 0 and 1, default 0.6(60%)
      */
     setMainRowHeight:(offset)=>{
-        if(!offset) offset=0.6;
+        if(!offset) offset=utils.mainRowOffset;
         let mainRowHeight=window.innerHeight*offset;
         $('#mainmap').height(mainRowHeight);
         $('#treeview').height(mainRowHeight);
@@ -23,5 +25,21 @@ var utils={
             text=$('#msg-errors').html().trim()+t;
         }
         $('#msg-errors').html(text + (msg?msg:'&nbsp;'));
+    },
+
+    /**
+     * Open a modal window to display a PDF file
+     * @param {*} file, the name and path of file
+     */
+    displayPdf:(file)=>{
+        $('#pdffile').height(0)
+        $('#display-pdf').modal('show');
+        window.setTimeout(
+            ()=>{
+                let height=parseInt((window.innerHeight-$('.modal-dialog').height())*0.95);
+                $('#pdffile').height(height);
+                $('#pdffile').attr('src',file);
+            },200
+        );
     }
 };
